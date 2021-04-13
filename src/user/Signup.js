cb5a32f9bf1c6780cacfc3b7ce4f90f778e975c2
Shaxadhere <Shaxad.here@gmail.com>
2021-04-13 12:41:41 +0500

    sign up

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -2,10 +2,19 @@ import React from 'react'
 import Layout from '../core/Layout'
 import {API} from '../config'
 
-const Signup = () => (
-    <Layout title="Signup" description="Signup to Node React E-Commerce App">
-        {API}
-    </Layout>
-)
+const Signup = () => {
+    const signUpForm = () => {
+        <form>
+            <div className="form-group">
+                <lable htmlFor="text-muted"></lable>
+            </div>
+        </form>
+    }
+
+    return (
+        <Layout title="Signup" description="Signup to Node React E-Commerce App"> 
+        </Layout>
+    )
+}
 
 export default Signup
