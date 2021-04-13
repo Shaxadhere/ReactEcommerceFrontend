8cd987396a78220884f5dc9e73a85ae5dde9de1b
Shaxadhere <Shaxad.here@gmail.com>
2021-04-13 12:30:38 +0500

    config and env

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Layout from '../core/Layout'
+import {API} from '../config'
 
 const Signup = () => (
     <Layout title="Signup" description="Signup to Node React E-Commerce App">
-        {process.env.REACT_APP_API_URL}
+        {API}
     </Layout>
 )
 
