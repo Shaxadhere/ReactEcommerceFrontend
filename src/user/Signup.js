44aaf343205ef1e9afe3239a8563b542fd6e129a
Shaxadhere <Shaxad.here@gmail.com>
2021-04-14 08:22:49 +0500

    sign up

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import Layout from '../core/Layout'
-import {API} from '../config'
+// import {API} from '../config'
 
 const Signup = () => {
-    const signUpForm = () => {
+    const signUpForm = () => (
         <form>
             <div className="form-group">
-                <lable htmlFor="text-muted"></lable>
+                <lable htmlFor="text-muted">Name</lable>
+                <input type="text" className="form-control" />
             </div>
+            <div className="form-group">
+                <lable htmlFor="text-muted">Email</lable>
+                <input type="email" className="form-control" />
+            </div>
+            <div className="form-group">
+                <lable htmlFor="text-muted">Password</lable>
+                <input type="password" className="form-control" />
+            </div>
+            <button type="button" className="btn btn-primary">Submit</button>
         </form>
-    }
+    )
 
     return (
-        <Layout title="Signup" description="Signup to Node React E-Commerce App"> 
+        <Layout className="container col-md-8 offset-md-2" title="Signup" description="Signup to Node React E-Commerce App"> 
+            {signUpForm()}
         </Layout>
     )
 }
