dcec1962a2deacf28030597cca3a3029f1ef3528
Shaxadhere <Shaxad.here@gmail.com>
2021-04-14 08:29:13 +0500

    sign up handle change input fields

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Layout from '../core/Layout'
 // import {API} from '../config'
 
 const Signup = () => {
+
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error: '',
+        success: false
+    })
+
+    const handleChange = name => event => {
+        setValues({...values, error: false, [name]: event.target.value})
+    }
+
     const signUpForm = () => (
         <form>
             <div className="form-group">
                 <lable htmlFor="text-muted">Name</lable>
-                <input type="text" className="form-control" />
+                <input onChange={handleChange('name')} type="text" className="form-control" />
             </div>
             <div className="form-group">
                 <lable htmlFor="text-muted">Email</lable>
-                <input type="email" className="form-control" />
+                <input onChange={handleChange('email')} type="email" className="form-control" />
             </div>
             <div className="form-group">
                 <lable htmlFor="text-muted">Password</lable>
-                <input type="password" className="form-control" />
+                <input onChange={handleChange('password')} type="password" className="form-control" />
             </div>
             <button type="button" className="btn btn-primary">Submit</button>
         </form>
@@ -24,6 +37,7 @@ const Signup = () => {
     return (
         <Layout className="container col-md-8 offset-md-2" title="Signup" description="Signup to Node React E-Commerce App"> 
             {signUpForm()}
+            {JSON.stringify(values)}
         </Layout>
     )
 }
